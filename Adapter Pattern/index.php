2d1b66e312cda350b07ec14a0856045f84572a31
rakib-09 <rakib.rumi09@gmail.com>
2020-05-02 00:53:48 +0600

    added adapter pattern

---
+++ Adapter Pattern/index.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by Anonymous
+ * Date: 2/5/20
+ * Time: 12:38 am
+ */
+
+interface Share {
+    public function shareData();
+}
+
+class WhatsAppShare {
+//    public function waShare(String $string)
+//    {
+//        echo "showing via whatsApp: {$string} \n";
+//    }
+    public function whatsAppShare(String $string)
+    {
+        echo "new way to show whatsApp message". $string;
+    }
+}
+
+class WhatsAppShareAdapter implements Share {
+    private $whatsApp;
+    private $data;
+    public function __construct(WhatsAppShare $whatsApp, String $message)
+    {
+        $this->whatsApp= $whatsApp;
+        $this->data = $message;
+    }
+
+    public function shareData()
+    {
+     //  $this->whatsApp->waShare($this->data);
+        //there is nothing we need to change without Adapter.
+        // just change in the adapter can keep all others remain same.
+        $this->whatsApp->whatsAppShare($this->data);
+    }
+}
+
+function clientCode (Share $share){
+    $share->shareData();
+}
+
+$adapter = new WhatsAppShareAdapter(new WhatsAppShare(), 'Hello from WhatsApp');
+clientCode($adapter);
+//now if we change whatsApp share method. it will not impact on our client side.
+// all the uses of adapter will remain same.
+//here is the example:
